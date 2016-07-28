4da789b0735832de094851896b0009b1d11b977b
Arnaud Taffanel <arnaud@bitcraze.se>
2016-07-28 19:28:59 +0200

    Added launchfiles and script to fly the new Crazyflie EKF

---
+++ scripts/crazyflie_controller_bridge.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# Make the Crazyflie internal position controller accessible the same way as
+# the Crazyflie ROS package demo controller
+
+# TODO: Read PID parameters and set them in the Crazyflie parameters
+
+import rospy
+from geometry_msgs.msg import PoseStamped, Twist
+from std_srvs.srv import Empty
+from crazyflie_driver.srv import UpdateParams
+
+# Takeoff and landing speed in ms^-1
+TAKEOFF_SPEED = 0.5
+LANDING_SPEED = 0.3
+UPDATE_RATE = 30
+
+
+class ControllerBridge:
+    def __init__(self):
+        self.target_setpoint = Twist()
+        self.landed_setpoint = Twist()
+        self.flying = False
+        self.taking_off = False
+        self.landing = False
+        self.transient_height = 0
+
+        self.goal_sub = rospy.Subscriber("goal", PoseStamped, self.new_goal)
+        self.cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+
+        # Services
+        self.ts = rospy.Service('takeoff', Empty, self.takeoff)
+        self.ls = rospy.Service('land', Empty, self.land)
+        self._update_params = rospy.ServiceProxy('update_params', UpdateParams)
+
+        # Send position setpoints 30 times per seconds
+        self.timer = rospy.Timer(rospy.Duration(1.0/UPDATE_RATE),
+                                 self.send_setpoint)
+
+    def run(self):
+        rospy.spin()
+
+    def send_setpoint(self, event):
+        if self.flying:
+            if self.taking_off:
+                sp = Twist()
+                sp.linear.x = self.target_setpoint.linear.x
+                sp.linear.y = self.target_setpoint.linear.y
+                sp.linear.z = self.transient_height*1000.0
+                self.cmd_vel_pub.publish(sp)
+                self.transient_height += TAKEOFF_SPEED/UPDATE_RATE
+                if sp.linear.z >= self.target_setpoint.linear.z:
+                    self.transient_height = 0
+                    self.taking_off = False
+            elif self.landing:
+                sp = Twist()
+                sp.linear.x = self.target_setpoint.linear.x
+                sp.linear.y = self.target_setpoint.linear.y
+                sp.linear.z = self.transient_height*1000.0
+                self.cmd_vel_pub.publish(sp)
+                self.transient_height -= LANDING_SPEED/UPDATE_RATE
+                if self.transient_height <= 0:
+                    self.transient_height = 0
+                    self.landing = False
+                    self.flying = False
+            else:
+                self.cmd_vel_pub.publish(self.target_setpoint)
+        else:
+            self.cmd_vel_pub.publish(self.landed_setpoint)
+
+    def new_goal(self, goal):
+        self.target_setpoint.linear.x = goal.pose.position.x
+        self.target_setpoint.linear.y = goal.pose.position.y
+        if goal.pose.position.z >= 0:
+            self.target_setpoint.linear.z = goal.pose.position.z*1000
+        else:
+            self.target_setpoint.linear.z = 1
+            rospy.logwarn("Cannot set <=0 Z setpoint!")
+
+    def takeoff(self, req):
+        rospy.set_param("flightmode/posSet", 1)
+        self._update_params(["flightmode/posSet"])
+        if self.landing or (not self.flying):
+            self.flying = True
+            self.taking_off = True
+            self.landing = False
+        return ()
+
+    def land(self, req):
+        if self.flying:
+            self.taking_off = False
+            self.landing = True
+        self.transient_height = self.target_setpoint.linear.z/1000.0
+        return ()
+
+if __name__ == "__main__":
+    rospy.init_node("controller_bridge")
+    cb = ControllerBridge()
+    cb.run()
---
+++ scripts/lps_ekf_bridge.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# Behaves like the other estimators but receives the position estimate from the
+# Crazyflie internal EKF instead of calculating the estimate
+
+import rospy
+from geometry_msgs.msg import Point
+
+import tf
+
+from crazyflie_driver.msg import GenericLogData
+
+
+def callback(data):
+    pt = Point()
+    pt.x = data.values[0]
+    pt.y = data.values[1]
+    pt.z = data.values[2]
+
+    position_pub.publish(pt)
+
+    br = tf.TransformBroadcaster()
+    br.sendTransform((pt.x, pt.y, pt.z),
+                     tf.transformations.quaternion_from_euler(0, 0, 0),
+                     rospy.Time.now(),
+                     "base_link",
+                     "world")
+
+if __name__ == "__main__":
+    rospy.init_node('lps_ekf_bridge')
+
+    position_pub = rospy.Publisher("/crazyflie_position", Point, queue_size=10)
+
+    rospy.Subscriber("log_kfpos", GenericLogData, callback)
+
+    rospy.spin()
