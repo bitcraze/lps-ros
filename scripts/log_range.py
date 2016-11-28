ef0c3e5fdac0aa9d1106dc561be01333efefef2d
Stephan Brown <stephan.brown.07@gmail.com>
2016-11-29 05:03:13 +0900

    log_range: Add a ranging visulization.

--- scripts/log_range.py
+++ scripts/log_range.py
@@ -5,7 +5,19 @@
 import rospy
 from bitcraze_lps_estimator.msg import RangeArray
 from std_msgs.msg import Float32MultiArray
+from visualization_msgs.msg import MarkerArray, Marker
+from geometry_msgs.msg import PoseStamped, Vector3, Point
 
+from anchorpos import get_anchors_pos
+
+import math
+
+anchorpos = []
+last_pos = PoseStamped()
+
+def pose_cb(data):
+    global last_pos
+    last_pos = data
 
 def callback(data):
     ranging = RangeArray()
@@ -20,12 +32,69 @@ def callback(data):
 
     ranging_pub.publish(ranging)
 
+    marker_array = MarkerArray()
+    for i in range(6):
+        marker = Marker()
+        marker.header.frame_id = "/world"
+        marker.id = i+40
+        marker.type = 4
+        marker.lifetime = rospy.Duration(1.0)
+        marker.ns = rospy.get_namespace()
+        marker.action = 0
+        p = Point()
+        p.x = anchorpos[i][0]
+        p.y = anchorpos[i][1]
+        p.z = anchorpos[i][2]
+        marker.points.append(p)
+        dir_vect = Vector3()
+        global last_pos
+        dir_vect.x = last_pos.pose.position.x - anchorpos[i][0]
+        dir_vect.y = last_pos.pose.position.y - anchorpos[i][1]
+        dir_vect.z = last_pos.pose.position.z - anchorpos[i][2]
+        mag = math.sqrt(dir_vect.x**2 + dir_vect.y**2 + dir_vect.z**2)
+        dir_vect.x /= mag
+        dir_vect.y /= mag
+        dir_vect.z /= mag
+        #print(ranging.ranges[i], anchorpos[i])
+        #marker.pose.position.x = ranging.ranges[i]*dir_vect.x + anchorpos[i][0]
+        #marker.pose.position.y = ranging.ranges[i]*dir_vect.y + anchorpos[i][1]
+        #marker.pose.position.z = ranging.ranges[i]*dir_vect.z + anchorpos[i][2]
+        #marker.pose.orientation.x = 0
+        #marker.pose.orientation.y = 0
+        #marker.pose.orientation.z = 0
+        #marker.pose.orientation.w = 1
+        dist = Point()
+        dist.x = ranging.ranges[i]*dir_vect.x + anchorpos[i][0]
+        dist.y = ranging.ranges[i]*dir_vect.y + anchorpos[i][1]
+        dist.z = ranging.ranges[i]*dir_vect.z + anchorpos[i][2]
+        marker.scale.x = 0.005
+        if '0' in marker.ns:
+            marker.color.r = 1
+        elif '1' in marker.ns:
+            marker.color.g = 1
+        elif '2' in marker.ns:
+            marker.color.b = 1
+        marker.color.a = 0.5
+        marker.points.append(dist)
+        marker_array.markers.append(marker)
+
+    range_paths.publish(marker_array)
+
+
 if __name__ == "__main__":
     rospy.init_node('log_range')
 
     ranging_pub = rospy.Publisher("ranging", RangeArray, queue_size=10)
 
-    rospy.Subscriber(rospy.get_namespace()+"log_ranges",
+    rospy.Subscriber("log_ranges",
                      Float32MultiArray, callback)
+
+    anchor_dict = get_anchors_pos()
+    for name in anchor_dict:
+        anchorpos.append((anchor_dict[name][0], anchor_dict[name][1], anchor_dict[name][2]))
+
+    rospy.Subscriber("pose", PoseStamped, pose_cb)
+
+    range_paths = rospy.Publisher(rospy.get_namespace() + "range_paths", MarkerArray, queue_size=10)
 
     rospy.spin()
